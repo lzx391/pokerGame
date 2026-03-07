dff9652c84105bbd80a6746850c2ca784404ce1b
86132 <1933886418@qq.com>
2026-03-07 16:13:58 +0800

    新的项目

---
+++ src/main/java/com/example/mgdemoplus/front/vuex-demo/src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+{{this.$store.state.count}}
+    <button @click="add">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+methods:{
+    add(){
+      this.$store.state.count+=1
+    }
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
