9573d0fcd62162f5043ebe106b50d100bd57d2d6
86132 <1933886418@qq.com>
2026-03-07 18:32:26 +0800

    没别的

--- src/main/java/com/example/mgdemoplus/front/vuex-demo/src/App.vue
+++ src/main/java/com/example/mgdemoplus/front/vuex-demo/src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
-
+<!--    {{ this.$store.state.count }}-->
+    {{count}}
+    <button @click="add">+1</button>
   </div>
 </template>
 
 <script>
 
 
+import HelloWorld from "@/components/HelloWorld.vue";
+
 export default {
   name: 'App',
   components: {
 
+  },
+  computed:{
+    count(){
+      return  this.$store.state.count
+    }
+  },
+  methods: {
+    add() {
+      this.$store.commit("increment")
+    }
   }
 }
 </script>
