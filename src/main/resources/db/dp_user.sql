fc3f8ffe400550361fbce839c5e269c90b6b259b
86132 <1933886418@qq.com>
2026-04-08 11:36:20 +0800

    docker安全模块更新

--- src/main/resources/db/dp_user.sql
+++ src/main/resources/db/dp_user.sql
@@ -3,7 +3,7 @@
 CREATE TABLE `dp_user` (
                            `id` int NOT NULL AUTO_INCREMENT,
                            `nickname` varchar(10) NOT NULL,
-                           `password` varchar(8) DEFAULT NULL,
+                           `password` varchar(64) NOT NULL ,
                            `avatar_url` varchar(255) DEFAULT NULL,
                            PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
